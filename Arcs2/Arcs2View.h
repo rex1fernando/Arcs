291605ef892674a94fcbaca8f56d5aad91231de7
Rex Fernando <rex1fernando@gmail.com>
2013-08-16 15:06:25 -0500

    Initial Commit

---
+++ Arcs2/Arcs2View.h
@@ -0,0 +1,13 @@
+//
+//  Arcs2View.h
+//  Arcs2
+//
+//  Created by Margie Fernando on 8/16/13.
+//  Copyright (c) 2013 Margie Fernando. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Arcs2View : ScreenSaverView
+
+@end
