e240f86aee3b07487f1a0e448bb6fdd0c140084f
Rex Fernando <rex@s-164-67-210-188.resnet.ucla.edu>
2016-12-09 20:16:42 -0800

    Initial Commit

---
+++ Arcs3/Arcs3View.h
@@ -0,0 +1,13 @@
+//
+//  Arcs3View.h
+//  Arcs3
+//
+//  Created by Rex Fernando on 12/9/16.
+//  Copyright © 2016 Rex Fernando. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Arcs3View : ScreenSaverView
+
+@end
---
+++ Arcs3/Arcs3View.m
@@ -0,0 +1,52 @@
+//
+//  Arcs3View.m
+//  Arcs3
+//
+//  Created by Rex Fernando on 12/9/16.
+//  Copyright © 2016 Rex Fernando. All rights reserved.
+//
+
+#import "Arcs3View.h"
+
+@implementation Arcs3View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
